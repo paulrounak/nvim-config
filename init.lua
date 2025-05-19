7717c8710c0654776bb561b35a1adedb4f629dcd
duckyfied <rounakpaul007@gmail.com>
2025-05-20 00:01:54 +0530

    update to show absolute and relative number simultaneously

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
-vim.cmd("set relativenumber")
+vim.cmd("set number relativenumber")
 
 require("config.lazy")
 vim.cmd([[colorscheme tokyonight-moon]])
