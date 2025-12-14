3c3862a9a9a3655040aa4f8130d0bca50f506a88
duckyfied <rounakpaul007@gmail.com>
2025-12-15 01:30:46 +0530

    migrate from depricated to new code

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -18,24 +18,36 @@ return {
     config = function()
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.clangd.setup({
+      -- Register LSP configs
+      vim.lsp.config("lua_ls", {
         capabilities = capabilities,
       })
 
+      vim.lsp.config("clangd", {
+        capabilities = capabilities,
+      })
+
+      -- Enable servers
+      vim.lsp.enable({ "lua_ls", "clangd" })
+
+      -- Diagnostics config (unchanged)
       vim.diagnostic.config({
-        virtual_text = true, -- Show inline diagnostics
+        virtual_text = true,
         signs = true,
         underline = true,
         update_in_insert = false,
       })
 
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+      -- LSP-only keymaps via LspAttach
+      vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function(event)
+          local opts = { buffer = event.buf }
+
+          vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+          vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+          vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+        end,
+      })
     end,
   },
 }
