046d20909b00a6c8d5c29e525dccb160c0951075
duckyfied <rounakpaul007@gmail.com>
2025-12-15 01:31:30 +0530

    update config

--- init.lua
+++ init.lua
@@ -5,6 +5,7 @@ vim.cmd("set shiftwidth=2")
 vim.cmd("set number relativenumber")
 
 require("config.lazy")
+-- require("cfhelper")
 vim.cmd([[colorscheme tokyonight-moon]])
 
 vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>")
